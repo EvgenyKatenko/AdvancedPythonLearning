c6c326cf8be6c80125596579ed3b24e0c2169c2d
EvgenyKatenko <katenko.evgeny@gmail.com>
2024-07-25 18:45:33 +0000

    Added filtering routines

--- lists.py
+++ lists.py
@@ -3,3 +3,9 @@ print(mylist)
 
 mylist2 = [5, True, "Apple", "Apple"]
 print(mylist2)
+
+mylist3 = [1,2,3,4,5,6,7,8,9]
+def power2(x):
+    return x**2
+print(list(filter(lambda x: x >= 5, map(power2,mylist3))))
+print([x for x in mylist3 if x >=5])
