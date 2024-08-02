857a50d59bc4e992d3fdfc24f32976d4ada4f63f
EvgenyKatenko <katenko.evgeny@gmail.com>
2024-08-02 22:14:45 +0100

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,1 +1,27 @@
 #Dictionary: key-value pairs, unordered, mutable
+
+mydict = {"name" : "Evgeny", "age" : 35, "hometown" : "Khabarovsk"} 
+
+mydict2 = dict(name = "Max", age = 34, hometown = "Komsa")
+
+print(mydict)
+print(mydict2["name"])
+
+del mydict["name"]
+print(mydict)
+
+'''
+.pop
+.popitem
+'''
+
+if "name" in mydict2:
+    print(mydict2["name"])
+
+for k,v in mydict.items():
+    print(f'{k} : {v}')
+
+# copying: deep vs shallow
+# .copy() for deep or dict(...)
+
+# .update() for overwriting with another dict
