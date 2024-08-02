4ae8165b5b3ab95ccfe7508f8e1c352fa71eb72c
EvgenyKatenko <katenko.evgeny@gmail.com>
2024-08-02 19:55:22 +0100

    Tuples finished

--- tuples.py
+++ tuples.py
@@ -1,4 +1,35 @@
 # TUPLE: ordered, immutable, allows duplicate elements
 
+
+#Creation
 mytup1 = ("Max",77,"Russia")
 print(mytup1)
+
+mytup2 = tuple([1,2,3,4])
+print(mytup2)
+
+print(mytup2[1])
+
+#Elements
+
+for x in mytup1:
+    print(x)
+
+print(len(mytup2))
+print(mytup2.count(1))
+'''
+.index(elem)
+'''
+
+#tuple -> list
+mylist = list(mytup2)
+print(mylist)
+
+#slicing is the same as in lists
+
+#unpacking
+a,b,c = mytup1 
+print(b)
+
+d1, *d2 = mytup1
+print(d2)
